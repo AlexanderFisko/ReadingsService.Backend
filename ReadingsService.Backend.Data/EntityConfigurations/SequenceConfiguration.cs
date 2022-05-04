a4b9d126c8a9e34c38267b96ff98651e1eb000b5
Fisko Alexander <fisko.a.v@sblogistica.ru>
2022-05-04 13:47:36 +0700

    Bug fixes

--- ReadingsService.Backend.Data/EntityConfigurations/SequenceConfiguration.cs
+++ ReadingsService.Backend.Data/EntityConfigurations/SequenceConfiguration.cs
@@ -6,8 +6,5 @@ namespace ReadingsService.Backend.Data.EntityConfigurations;
 
 internal class SequenceConfiguration : IEntityTypeConfiguration<SequenceEntity>
 {
-    public void Configure(EntityTypeBuilder<SequenceEntity> builder)
-    {
-        // NOP
-    }
+    public void Configure(EntityTypeBuilder<SequenceEntity> builder) => builder.HasMany(s => s.Observations).WithOne(o => o.Sequence).OnDelete(DeleteBehavior.Cascade);
 }
